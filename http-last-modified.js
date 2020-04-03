b3bda1740c43a598a0e977b7d8e14fbb4f7ac36c
akira-cn <akira.cn@gmail.com>
2020-04-03 21:58:35 +0800

    update

--- http-last-modified.js
+++ http-last-modified.js
@@ -18,22 +18,20 @@ const server = http.createServer((req, res) => {
       const {ext} = path.parse(filePath);
       const stats = fs.statSync(filePath);
       const timeStamp = req.headers['if-modified-since'];
+      let status = 200;
       if(timeStamp && Number(timeStamp) === stats.mtimeMs) {
-        // 如果修改时间依然是last-modified时间，说明文件内容没有变化
-        res.writeHead(304, {
-          'Content-Type': mime.getType(ext),
-          'Cache-Control': 'max-age=86400', // 缓存一天
-          'Last-Modified': stats.mtimeMs,
-        });
-        res.end(); // 不发送body
-      } else {
-        res.writeHead(200, {
-          'Content-Type': mime.getType(ext),
-          'Cache-Control': 'max-age=86400', // 缓存一天
-          'Last-Modified': stats.mtimeMs,
-        });
+        status = 304;
+      }
+      res.writeHead(status, {
+        'Content-Type': mime.getType(ext),
+        'Cache-Control': 'max-age=86400', // 缓存一天
+        'Last-Modified': stats.mtimeMs,
+      });
+      if(status === 200) {
         const fileStream = fs.createReadStream(filePath);
         fileStream.pipe(res);
+      } else {
+        res.end();
       }
     }
   } else {
