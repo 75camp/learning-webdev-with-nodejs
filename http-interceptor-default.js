f451b3758f0618352b9ddec804332892ab597340
akira-cn <akira.cn@gmail.com>
2020-04-09 23:35:53 +0800

    update

--- http-interceptor-default.js
+++ http-interceptor-default.js
@@ -1,12 +1,27 @@
 const Server = require('./lib/server');
+const Router = require('./lib/middleware/router');
 
 const app = new Server();
 
-app.use(async ({res}, next) => {
+const router = new Router();
+
+app.use(router.all('/test/:course/:lecture', async ({route, res}, next) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.body = route;
+  await next();
+}));
+
+app.use(router.all('.*', async ({req, res}, next) => {
   res.setHeader('Content-Type', 'text/html');
   res.body = '<h1>Hello world</h1>';
   await next();
-});
+}));
+
+// app.use(async ({req, res}, next) => {
+//   res.setHeader('Content-Type', 'text/html');
+//   res.body = '<h1>Hello world</h1>';
+//   await next();
+// });
 
 app.listen({
   port: 9090,
